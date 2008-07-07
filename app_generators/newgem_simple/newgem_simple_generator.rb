af5bdc517ae4d6477f536ffda941cd64688e90c4
Jeffrey Damick <jdamick@Macintosh-92.local>
2008-07-07 11:19:16 -0400

    changed to use a template for test_helper

--- app_generators/newgem_simple/newgem_simple_generator.rb
+++ app_generators/newgem_simple/newgem_simple_generator.rb
@@ -26,10 +26,10 @@ class NewgemSimpleGenerator < RubiGen::Base
       m.file "lib/templates.rb", "lib/#{name}.rb"
       m.template "spec/templates_spec.rb.erb", "spec/#{name}_spec.rb"
       m.template "test/templates_test.rb.erb", "test/#{name}_test.rb"
-      
+      m.template "test/test_helper.rb.erb", "test/test_helper.rb"      
       
       %w[ LICENSE Rakefile README.rdoc ].each {|file| m.template file, file}
-      %w[ TODO spec/spec_helper.rb test/test_helper.rb ].each {|file| m.file file, file }      
+      %w[ TODO spec/spec_helper.rb ].each {|file| m.file file, file }      
       
       m.dependency "install_rubigen_scripts", [destination_root, 'newgem_simple'], 
         :shebang => options[:shebang], :collision => :force
